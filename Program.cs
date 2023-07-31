93b1558e98de615b7f2c707812384141b3a67c7e
Kydo Solis <kydo@gistlens.com>
2023-07-31 19:09:13 +0800

    Modify to allow copy of whole contents of the bucket for every 3hrs

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@ using Amazon.SecurityToken;
 using Amazon.SecurityToken.Model;
 using System;
 using System.Threading.Tasks;
+using System.Timers;
+using Timer = System.Timers.Timer;
 
 namespace S3CopyObjectCrossAccount
 {
@@ -14,17 +16,22 @@ namespace S3CopyObjectCrossAccount
         private static readonly RegionEndpoint destinationBucketRegion = RegionEndpoint.USWest2;
         private static IAmazonS3 s3Client;
 
-        private const string sourceBucket = "source-bucket-name";
-        private const string destinationBucket = "destination-bucket-name";
-        private const string objectKey = "object-key";
+        private
+        const string sourceBucket = "source-bucket-name";
+        private
+        const string destinationBucket = "destination-bucket-name";
 
-        private const string roleArn = "arn:aws:iam::destination-account-id:role/role-name";
+        private
+        const string roleArn = "arn:aws:iam::destination-account-id:role/role-name";
 
         static void Main()
         {
             s3Client = new AmazonS3Client(sourceBucketRegion);
             Console.WriteLine("Copying an object");
-            CopyingObjectAsync().Wait();
+            Timer timer = new Timer(10800000);
+            timer.Elapsed += async (sender, e) => await CopyingObjectAsync();
+            timer.Start();
+            Console.ReadLine();
         }
 
         private static async Task CopyingObjectAsync()
@@ -40,31 +47,65 @@ namespace S3CopyObjectCrossAccount
                 AssumeRoleResponse assumeResponse = await stsClient.AssumeRoleAsync(assumeRequest);
 
                 AmazonS3Client s3ClientWithRole = new AmazonS3Client(
-                    assumeResponse.Credentials,
-                    destinationBucketRegion
+                  assumeResponse.Credentials,
+                  destinationBucketRegion
                 );
-
-                CopyObjectRequest request = new CopyObjectRequest
+                ListObjectsV2Request listRequest = new ListObjectsV2Request
                 {
-                    SourceBucket = sourceBucket,
-                    SourceKey = objectKey,
-                    DestinationBucket = destinationBucket,
-                    DestinationKey = objectKey
+                    BucketName = sourceBucket
                 };
-                CopyObjectResponse response = await s3ClientWithRole.CopyObjectAsync(request);
+                ListObjectsV2Response listResponse;
+                do
+                {
+                    listResponse = await s3Client.ListObjectsV2Async(listRequest);
+                    foreach (S3Object obj in listResponse.S3Objects)
+                    {
+                        GetObjectMetadataRequest metadataRequest = new GetObjectMetadataRequest
+                        {
+                            BucketName = destinationBucket,
+                            Key = obj.Key
+                        };
+                        bool exists = true;
+                        try
+                        {
+                            GetObjectMetadataResponse metadataResponse = await s3ClientWithRole.GetObjectMetadataAsync(metadataRequest);
+                        }
+                        catch (AmazonS3Exception e)
+                        {
+                            if (e.StatusCode == System.Net.HttpStatusCode.NotFound)
+                                exists = false;
+                            else
+                                throw;
+                        }
+                        if (!exists)
+                        {
+                            CopyObjectRequest request = new CopyObjectRequest
+                            {
+                                SourceBucket = sourceBucket,
+                                SourceKey = obj.Key,
+                                DestinationBucket = destinationBucket,
+                                DestinationKey = obj.Key
+                            };
+                            CopyObjectResponse response = await s3ClientWithRole.CopyObjectAsync(request);
+                            Console.WriteLine("Copied object {0}", obj.Key);
+                        }
+                    }
+                    listRequest.ContinuationToken = listResponse.NextContinuationToken;
+                } while (listResponse.IsTruncated);
+
             }
             catch (AmazonS3Exception e)
             {
                 Console.WriteLine(
-                    "Error encountered on server. Message:'{0}' when writing an object",
-                    e.Message
+                  "Error encountered on server. Message:'{0}' when writing an object",
+                  e.Message
                 );
             }
             catch (Exception e)
             {
                 Console.WriteLine(
-                    "Unknown encountered on server. Message:'{0}' when writing an object",
-                    e.Message
+                  "Unknown encountered on server. Message:'{0}' when writing an object",
+                  e.Message
                 );
             }
         }
